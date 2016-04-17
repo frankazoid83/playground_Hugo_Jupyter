75748384aaa112b248060784278dbb1d9c45012a
frank-feulner <frank.feulner@aexea.de>
2016-04-17 18:51:04 +0200

    deploy.sh now adds variable front matter into the generated html files

--- testsite/script/deploy.sh
+++ testsite/script/deploy.sh
@@ -14,7 +14,6 @@
 for file in $contentpath*.ipynb ; do
 # Notebooks in html mit basistemplate umwandeln
 jupyter nbconvert $file to html --template basic
-
 done
 
 #Datum so umformatieren, dass hugo es erkennt --> Zeitzone +0200 zu +02:00
@@ -23,13 +22,19 @@ currenttime=${currenttime/0200/02:00}
 currenttime=${currenttime/0100/01:00}
 
 for file in $contentpath*.html ; do
-# front matter einfügen
-sed -e '1s/.*$/+++\n\&&/g' $file
-sed -e '1s/.*$/title = \"notebook4\"\n\&&/g' $file
-sed -e '1s/.*$/draft = true\n\&&/g' $file
-sed -e '1s/.*$/date = "$currenttime"\n\&&/g' $file
-sed -e '1s/.*$/+++\n\&&/g' $file
+  #Datum so umformatieren, dass hugo es erkennt --> Zeitzone +0200 zu +02:00
+  currenttime=$(date +%Y-%m-%dT%H:%M:%S%z)
+  currenttime=${currenttime/0200/02:00}
+  currenttime=${currenttime/0100/01:00}
+  #Name der aktuellen Datei auslesen und für Titel verwenden
+  currentfilename=$(basename "$file")
+  currentfilename=${currentfilename/.html/}
 
+  sed -iold '1i\'$'\n''+++'$'\n' $file
+  sed -iold '1i\'$'\n''title = "'$currentfilename'"'$'\n' $file
+  sed -iold '1i\'$'\n''draft = true'$'\n' $file
+  sed -iold '1i\'$'\n''date = "'$currenttime''$'\n' $file
+  sed -iold '1i\'$'\n''+++'$'\n' $file
 # konvertierte Dateien ins Contentverzeichnis von Hugo schieben
-echo "schleife 2"
+  
 done
